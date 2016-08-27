919bcd51bbca0ddb55a568cbe500a73f71aa0de9
andrey <a.gritsik@gmail.com>
2016-08-27 21:41:04 +0300

    Add requestor and responder microservices

---
+++ requestor/src/test/java/com/agritsik/ds/requestor/RequestorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agritsik.ds.requestor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RequestorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ responder/src/main/java/com/agritsik/ds/requestor/ResponderApplication.java
@@ -0,0 +1,12 @@
+package com.agritsik.ds.requestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponderApplication.class, args);
+	}
+}
