80f6e3f180763a772f66076a2f9df4d73703a387
andrey <a.gritsik@gmail.com>
2016-08-28 18:33:03 +0300

    Close #5. Add Service Registry

--- requestor/src/main/java/com/agritsik/ds/requestor/RequestorApplication.java
+++ requestor/src/main/java/com/agritsik/ds/requestor/RequestorApplication.java
@@ -2,18 +2,20 @@ package com.agritsik.ds.requestor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RequestorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RequestorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RequestorApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
